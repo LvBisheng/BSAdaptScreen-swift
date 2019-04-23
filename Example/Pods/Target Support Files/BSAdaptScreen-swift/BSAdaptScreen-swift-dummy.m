a5e1574ff098b66dce627c4dacacd88327285fff
lvbisheng <lvbisheng@swipal.com>
2019-04-23 15:21:32 +0800

    Xxxx

---
+++ Example/Pods/Target Support Files/BSAdaptScreen-swift/BSAdaptScreen-swift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BSAdaptScreen_swift : NSObject
+@end
+@implementation PodsDummy_BSAdaptScreen_swift
+@end
---
+++ Example/Pods/Target Support Files/Pods-BSAdaptScreen-swift-demo/Pods-BSAdaptScreen-swift-demo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BSAdaptScreen_swift_demo : NSObject
+@end
+@implementation PodsDummy_Pods_BSAdaptScreen_swift_demo
+@end
